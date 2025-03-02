e7dda9bd56a049e6cbc25037ad17d587d4e60999
mateo <letertre@et.esiea.fr>
2025-03-02 20:05:40 +0100

    Add database logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1>home page content</h1>
